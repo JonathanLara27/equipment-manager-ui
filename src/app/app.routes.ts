eb4cce17e070c3fc0f2dc73009e558166f157ad2
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2026-01-12 19:16:27 -0500

    Add title to dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./shared/components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+        loadComponent: () => import('./shared/components/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        title: 'Dashboard'
     }
 ];
